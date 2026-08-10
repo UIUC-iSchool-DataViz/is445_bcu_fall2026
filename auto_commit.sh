556575ca31fd29fa78fee6fc3e42190a5cfe2a06
jnaiman <jnaiman.illinois.edu>
2026-08-10 12:25:33 -0500

    updating

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MESSAGE="Auto-commit: $(date)"
-REPO_PATH="/Users/jnaiman/is445_obuobg_spring2026"
+REPO_PATH="/Users/jnaiman/is445_bcu_fall2026"
 cd $REPO_PATH
 /usr/bin/git add -A
 /usr/bin/git commit -m "$MESSAGE"
